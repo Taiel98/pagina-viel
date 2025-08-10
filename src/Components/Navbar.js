85d1df3471038095164b0eccd083925b90b2f87c
Taiel98 <taielrodrigok@gmail.com>
2025-08-10 09:59:33 -0300

    agregando menu responsive

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,6 +6,21 @@ function Navbar(){
         <>
         <nav className="navbar navbar-expand-lg navbar-dark ">
             <div className="container-fluid">
+                 {/* Botón hamburguesa para móviles */}
+                <button 
+                    className="navbar-toggler" 
+                    type="button" 
+                    data-bs-toggle="collapse" 
+                    data-bs-target="#navbarNav" 
+                    aria-controls="navbarNav" 
+                    aria-expanded="false" 
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                
+                {/* Menú colapsable */}
+                <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav mb-2 mb-lg-0 nav justify-content-end">
                     <li className="nav-item">
                         <div>
@@ -33,6 +48,7 @@ function Navbar(){
                         </div>
                     </li>
                 </ul>
+                </div>
             </div> 
         </nav>
         </>
